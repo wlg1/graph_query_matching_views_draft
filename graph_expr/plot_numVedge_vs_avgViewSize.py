128d1d522e842355df3f8d597a0641bb77920a5c
wlg1 <35532570+wlg1@users.noreply.github.com>
2021-06-08 10:03:55 -0400

    plot_numVedge_vs_avgViewSize.py

---
+++ graph_expr/plot_numVedge_vs_avgViewSize.py
@@ -0,0 +1,62 @@
+import os, csv
+import matplotlib.pyplot as plt
+
+def RepresentsInt(s):
+    try: 
+        int(s)
+        return True
+    except ValueError:
+        return False
+    
+numqrys = 14
+totNumE = 3
+algoNames = ['simgrBYVIEWS_rmvEmpty', 'simgrBYVIEWS']
+tot_ByEdge = []
+for numE in range(2,totNumE + 1):
+    algo_avgViewSizes = {}
+    for algoN in algoNames:
+        algo_avgViewSizes[algoN] = [0] * numqrys
+    input_path = 'output/Email_lb20_lb20_cyc_m_'+str(numE)+'Eviews/'
+    for filename in os.listdir(input_path):
+        if 'VIEWS' not in filename:
+            continue
+        filename_as_list = filename.split('_')
+        if 'rmvEmpty' in filename:
+            algoN = filename_as_list[8] + "_rmvEmpty"
+        else:
+            algoN = filename_as_list[8]
+        qrynum = int(filename_as_list[6].replace('q','')) - 6
+        file = open(input_path+filename, 'r')
+        reader = csv.reader(file, delimiter=',')
+        startRead = False
+        for row in reader:
+            if 'View' in row[0]:
+                startRead = True
+            if startRead and RepresentsInt(row[0]):
+                algo_avgViewSizes[algoN][qrynum] = float(row[-1])
+            elif startRead and '*' in row[0]:
+                break
+    tot_ByEdge.append(algo_avgViewSizes)
+
+#plot edges vs algoTotTime. each algo is a separate line in plot
+xAxis =  [str(x+1)+'E' for x in list(range(1,totNumE))]
+for algoName in algoNames:
+    yAxis = []
+    for algo_avgViewSizes in tot_ByEdge:
+        algoVsize = algo_avgViewSizes[algoName]
+        yAxis.append(sum(algoVsize) / len(algoVsize))
+    plt.plot(xAxis, yAxis, label = algoName)
+plt.legend(bbox_to_anchor=(1, 1), loc='upper left')
+plt.savefig('avgViewSizes.png', bbox_inches="tight")
+
+for qrynum in range(1,13):
+    plt.figure()
+    for algoName in algoNames:
+        yAxis = []
+        for algo_avgViewSizes in tot_ByEdge:
+            algoVsize = algo_avgViewSizes[algoName]
+            yAxis.append(algoVsize[qrynum])
+        plt.plot(xAxis, yAxis, label = algoName)
+    plt.legend(bbox_to_anchor=(1, 1), loc='upper left')
+    plt.savefig('q'+str(qrynum+6)+'_ViewSizes.png', bbox_inches="tight")
+        
